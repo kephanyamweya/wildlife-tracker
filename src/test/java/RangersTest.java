e0fb9ba2a2a1a950085ef74b3e9724257deefb3a
kephanyamweya <kephanyamweya95@gmail.com>
2021-08-25 15:39:27 +0300

    add rangers hbs to create rangers

---
+++ src/test/java/RangersTest.java
@@ -0,0 +1,95 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+public class RangersTest {
+    @Test
+    public void createInstanceOfRangersClass_true(){
+        Rangers ranger= setUpNewRanger();
+        assertEquals(true,ranger instanceof Rangers);
+    }
+
+    @Test
+    public void allEntriesAreSaved() {
+        Rangers ranger= setUpNewRanger();
+        ranger.save();
+        assertTrue(Rangers.all().get(0).equals(ranger));
+
+    }
+
+    @Test
+    public void emptyFieldsAreNotSaved() {
+        Rangers ranger=new Rangers("","","0703874756");
+        try{
+            ranger.save();
+            assertTrue(Rangers.all().get(0).equals(ranger));
+        }catch (IllegalArgumentException e){
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    public void findById() {
+        Rangers ranger= setUpNewRanger();
+        Rangers otherRanger=new Rangers("Ken","2","0708254563");
+        ranger.save();
+        otherRanger.save();
+        Rangers foundRanger=Rangers.find(ranger.getId());
+        assertTrue(foundRanger.equals(ranger));
+
+    }
+
+    @Test
+    public void entryIsUpdatedCorrectly() {
+        Rangers ranger= setUpNewRanger();
+        Rangers otherRanger=ranger;
+        ranger.save();
+        try {
+            ranger.update(ranger.getId(),"Justine","0758605603");
+            Rangers foundRanger=Rangers.find(ranger.getId());
+            assertNotEquals(foundRanger,otherRanger);
+            assertEquals(foundRanger.getId(),otherRanger.getId());
+
+        }catch (IllegalArgumentException e){
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    public void entriesAreDeleted() {
+        Rangers ranger= setUpNewRanger();
+        Rangers otherRanger=new Rangers("Ken","2","0708254653");
+        ranger.save();
+        otherRanger.save();
+        ranger.delete();
+        assertEquals(null,Rangers.find(ranger.getId()));
+
+    }
+
+    @Test
+    public void allSightingsAreReturnedForRanger() {
+        Rangers ranger=setUpNewRanger();
+        try {
+            Locations location=new Locations("Zone A");
+            ranger.save();
+            location.save();
+            Sightings sighting=new Sightings(location.getId(),ranger.getId(),1);
+            Sightings otherSighting=new Sightings(1,ranger.getId(),1);
+            sighting.save();
+            otherSighting.save();
+            assertEquals(ranger.getRangerSightings().get(0),sighting);
+            assertEquals(ranger.getRangerSightings().get(1),otherSighting);
+        }catch (IllegalArgumentException e){
+            System.out.println(e);
+        }
+
+    }
+
+    //helper class
+    private Rangers setUpNewRanger() {
+        return new Rangers("Myrrh","1","07564378");
+    }
+
+
+}
