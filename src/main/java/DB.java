9881067941cac2a85bb76ba22fe4709015dd0116
kephanyamweya <kephanyamweya95@gmail.com>
2021-08-26 09:58:01 +0300

    add new images to the project images folder

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -8,8 +8,5 @@ import java.sql.SQLException;
 public class DB {
     public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "kepha", "0703874756");
 }
-//    public DB() throws SQLException {
-//    }
-//
-//    public static Sql2o sql2o = new Sql2o("jdbc:postgres://ec2-52-72-125-94.compute-1.amazonaws.com:5432/d3qcmcv58u09tu", "uxcsgwdlgfzrlq", "ab76dda5b51f71a85449094811f776d454e4e1476fc35add721b52c3d7992438");
-//}
+
+//postgres://eqxnmiwdiwflst:4adb232261ef2e7346b4dfd468d143a65c7f9b86c4d9e4bd457f4db2db10d298@ec2-54-156-60-12.compute-1.amazonaws.com:5432/d4usvin9sjtdck);
